f5b3a7ff473ef83d6af0fa2ac667b0eaea13fa92
PAnd0509 <paola2010andrea05@gmail.com>
2024-02-25 17:11:26 -0500

    Funcionalidad de las operaciones

--- main.py
+++ main.py
@@ -16,4 +16,15 @@ if option not in [1,2,3]:
     print ("Valor no valido")
     exit()
 
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
 
+if option == 1:
+    print("El resultado de la suma es:", suma(num1, num2))
+elif option == 2:
+    print("El resultado de la resta es:", resta(num1, num2))
+elif option == 3:
+    if num2 == 0:
+        print("No se puede dividir por cero.")
+    else:
+        print("El resultado de la división es:", division(num1, num2))
